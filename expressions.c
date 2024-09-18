4a5991ac0abb462303c3e32c1d69827f8e94e95f
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-09-18 13:21:01 -0600

    Update expressions.c

--- expressions.c
+++ expressions.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(void)
+{
+ int a = (7-24/8*4+6);
+ int b = (18/3-7+2*5);
+ int c = (6*4/12+72/8-9);
+ int d = ((17-6/2)+4*3);
+ int e = (-2*(1*4-2/2)+(6+2-3));
+ int f = (-1*((3-4*7)/5)-2*24/6);
+ int g = (3*(5*5)/15)-(5-(2*2));
+ int h = (pow(1,4)*(pow(2,2))+(pow(3,3))-(pow(2,5))/4);
+ int i = ((pow(22/2-2*5,2))+(pow(4-6/6,2)));
+
+printf("%d \n", a);  
+printf("%d \n", b); 
+printf("%d \n", c);    
+printf("%d \n", d);
+printf("%d \n", e);
+printf("%d \n", f);
+printf("%d \n", g);
+printf("%d \n", h);
+printf("%d \n", i);
+}
