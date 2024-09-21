91c301ce1357dcb56e5bdbce4b750cd34c005ef8
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-09-21 12:41:50 -0600

    Update sillysentences.py

--- sillysentences.py
+++ sillysentences.py
@@ -1,1 +1,6 @@
 
+print("hello and welcome to silly sentences")
+animal = input("what is the thing doing the action?: \n")
+verb = input("what is it doing?: \n")
+place = input("where is the place this is taking place?: \n")
+print("the", animal, "was", verb,"in", place)
