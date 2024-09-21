bf71adfb30c98c6d5cbeed702bee87d1de824789
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-09-21 12:53:36 -0600

    Update nameDecorater.c

--- nameDecorater.c
+++ nameDecorater.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char name[9] = "sawyer";
+    char prefix[9] = ":)";
+    char prefics[9] = "(:";
+    strcat(prefix, name);
+    strcat(name, prefics);
+    printf("%s", prefix); printf("%s", prefics);
+
+return 0;
+}
