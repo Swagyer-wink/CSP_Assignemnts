c506c3238862bd339d10da54583599273cbad26d
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-09-23 13:15:15 -0600

    Update sillySentences.c

--- sillySentences.c
+++ sillySentences.c
@@ -1,1 +1,15 @@
-#include <Stdio.h>
+#include <stdio.h>
+
+int main(void){
+    char animal[20];
+    char place[20];
+    char pastVerb[20];
+    printf("Name an animal: ");
+    scanf("%s", animal );
+    printf("name a place: ");
+    scanf("%s", place);
+    printf("name a pastVerb: ");
+    scanf("%s", pastVerb);
+    printf("the %s walked to the %s and %s with his friend the anger turtle. ", animal, place, pastVerb);
+    return 0;
+}
