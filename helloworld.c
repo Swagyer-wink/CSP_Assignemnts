01a4f0175e9421c28c6c7b19cb1c34c1fee78ba5
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-09-24 13:32:04 -0600

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void name(char name[]){
+    printf("hello %s\n", name);
+}
+
+int main(void){
+    name("maren");
+    name("david marteniz");
+    name("dqauirs");
+    name("micheal alenglo");
+    name("spondge bob");
+    return 0;
+}
