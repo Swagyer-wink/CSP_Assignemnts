29baa3145eb7a310a10b31aca0a63768863768fc
Swagyer-wink <sawyer.winkelkotter@ucas-edu.net>
2024-10-22 13:06:50 -0600

    Update update_budget.py

--- update_budget.py
+++ update_budget.py
@@ -1,28 +1,24 @@
-
 print("hello and welcome to your financail caculator!")
-income = float(input("how much do you make a month?:\n"))
-rent = float(input("how much is your rent?:\n"))
-utilities = float(input("what is your utilities cost?:\n"))
-groceries = float(input("how much does your groceries cost?:\n"))
-trasnportation = float(input("how much does your trasportation cost:\n"))
+def value(person, name):
+    print("Your monthly", name, "is $", person)
+    return
+
+income = int(input("how much do you make a month?:\n"))
+rent = int(input("how much is your rent?:\n"))
+utilities = int(input("what is your utilities cost?:\n"))
+groceries = int(input("how much does your groceries cost?:\n"))
+trasnportation = int(input("how much does your trasportation cost:\n"))
 savings = income*0.2
 expenses= rent + utilities + groceries + trasnportation
 spending = income-expenses-savings
 def percent(type, amount):
     per = amount/income * 100
     
-    return f"Your {type} is {per}% income."
-print("you make", income)
-print("your expenses are", expenses)
-print("your rent is", rent)
-print("your utilities are", utilities)
-print("your groceries are", groceries)
-print("your trasnportation cost is", trasnportation)
-prent = (float(rent)/float(income))*100
-putilities = (float(utilities)/float(income))*100
-pgroceries = (float(groceries)/float(income))*100
-ptrasnportation = (float(trasnportation)/float(income))*100
-print("your rent is", prent)
-print("your uitlities is", putilities)
-print("your groceries is", pgroceries)
-print("your trasnportation is", ptrasnportation)
+    return f"Your {type} is {per}%."
+
+print(percent("income", income))
+print(percent("expenses", expenses))
+print(percent("rent", rent))
+print(percent("utilities", utilities))
+print(percent("groceries", groceries))
+print(percent("trasnportation", trasnportation))
